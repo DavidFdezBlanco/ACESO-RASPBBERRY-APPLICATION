099b5a6f6c2759c2afaa392332366c6818dc92be
DavidMainAccount <DavidMainAccount>
2020-01-07 19:50:37 +0000

    wow

--- LoRaWAN/raspi.py
+++ LoRaWAN/raspi.py
@@ -2,14 +2,18 @@
 """
     Test harness for dragino module - sends hello world out over LoRaWAN 5 times
 """
+import time
+import datetime
+import random
 import logging
+import os
 from time import sleep
 import RPi.GPIO as GPIO
 from libs.dragino import Dragino
 
-max_size = 200  #max bytes size supported by lorawan protocol. 2 bytes from i
+max_size = 150  #max bytes size supported by lorawan protocol. 2 bytes from i
 min_size = 40  #kinda arbitrary
-
+lastindex = 1
 realTimeBatch = [None] * 800
 
 #returns an array of blocks
@@ -29,13 +33,18 @@ def fragment(SF, file):
     else:
         chunk_size = min_size
 
-    print("Chunk size is " + str(chunk_size))
+#    print("Chunk size is " + str(chunk_size))
     output = []  #The fragmented file
     packetID = 1
-    hash = hash(datetime.now())
+    packetHash = random.randint(1, 10000)
+    with open(file,'r') as fileToSave:
+        with open("sentFiles/"+str(packetHash)+".txt","w") as fileToWrite:
+            dataToSave = fileToSave.read()
+            fileToWrite.write(dataToSave)
+
     with open(file,'rb') as ifile:
         while True:
-            prefix = "[IDPCK:" + str(packetID) + "]" + "[" + hash + "]!"
+            prefix = "[IDPCK:" + str(packetID) + "]" + "[" + str(packetHash)  + "]!"
             prefix_as_bytes = str.encode(prefix)
             data = ifile.read(chunk_size)
             if data.decode() == "":
@@ -49,14 +58,14 @@ def utf8len(s):
     return len(s.encode('utf-8'))
 
 def getRealSF(s):
-    if(s == 3):
+    if(s == 5):
         return 12
     if(s == 4):
         return 10
-    if(s == 5):
+    if(s == 3):
         return 7
 
-def send_file_retransmission_static_size(file, SF): #modify according to protocl
+def send_file_retransmission_static_size(D,file, SF): #modify according to protocl
     fragmented_file = fragment(SF, file)
     i = 1
     for block in fragmented_file:
@@ -65,7 +74,7 @@ def send_file_retransmission_static_size(file, SF): #modify according to protocl
         i+=1
         sleep(1)
 
-def send_file_redondance_static_size(file, numberOfRetrans, SF): #send file
+def send_file_redondance_static_size(D,file, numberOfRetrans, SF): #send file
     fragmented_file = fragment(SF, file)
     i = 1
     for block in fragmented_file:
@@ -76,7 +85,7 @@ def send_file_redondance_static_size(file, numberOfRetrans, SF): #send file
         print("block " + str(i) + "/" + str(len(fragmented_file)) + " has been sent")
         i+=1
     
-def send_file_historique_static_size(file, historiqueWindow, SF):
+def send_file_historique_static_size(D, file, historiqueWindow, SF):
     fragmented_file = fragment(SF, file)
     i = 1
     sending = [None]*historiqueWindow
@@ -92,12 +101,26 @@ def send_file_historique_static_size(file, historiqueWindow, SF):
         
 GPIO.setwarnings(False)
 
+
 def consumeRealTimeBatch(D):
-    for trace in realTimeBatch:
-        if ((str(None) in str(trace) == False):
-            D.send(str(trace))
-            trace = None
-    
+    global lastindex
+    packetHash = random.randint(1, 10000)
+    prefix = "[REALTIME]" + "[" + str(packetHash)  + "]!"
+    data = int(round(time.time() * 1000))
+    date2 = datetime.datetime.fromtimestamp(data/1000.0)
+    date = date2.strftime('%Y-%m-%d %H:%M:%S')
+    value = 80 - random.randint(0, 10)
+    message = "[Pulse];["+str(lastindex)+"] " + str(value)+ ",sensor1,"+date+"\n" 
+    D.send(prefix + message)
+    sleep(0.3)
+    D.send(prefix + message)
+    sleep(0.3)
+    D.send(prefix + message)
+    sleep(0.3)
+    lastindex += 1
+    file1 = open("filesToSend/data.txt","a")
+    file1.write(message)
+    print("***Real time message"+str(lastindex)+" sent")
     
 def getOptimalChannelSF():
     D = Dragino("configs/dragino"+str(7)+".ini", logging_level=logging.WARN)
@@ -112,9 +135,9 @@ def getOptimalChannelSF():
     return getRealSF(optimalIndex)
 
 def stablish_session():
-    SF = getOptimalChannelSF():
+    SF = getOptimalChannelSF()
     print("Restarting channel with sf" + str(SF))
-    D = Dragino("configs/dragino"+str(SF)+".ini", logging_level=logging.INFO)
+    D = Dragino("configs/dragino"+str(SF)+".ini", logging_level=logging.WARN)
     D.join()
     
     while not D.registered():
@@ -125,13 +148,12 @@ def stablish_session():
     numberOfSends = 0
     while numberOfSends < 20:
         numberOfSends += 1
-        #consumeRealTimeBatch(D)
-        if (numberOfSends == 20):
-            send_file_redondance_static_size("libs/large.txt",3, SF)
-        sleep(0.3)
+        consumeRealTimeBatch(D)
+        if (numberOfSends == 10 or numberOfSends == 20):
+            send_file_redondance_static_size(D,"filesToSend/data.txt",4, SF)
+            os.remove("filesToSend/data.txt")
     GPIO.cleanup()
 
-    
 
-
-stablish_session():
+while True:
+    stablish_session()
